69d9b0c7ec111dab8d188dab02e34ab393dfe704
tm8619 <matsuzaki_toshiya8619@yahoo.co.jp>
2024-05-26 22:31:49 +0900

    team

---
+++ terraform/teams.tf
@@ -0,0 +1,21 @@
+resource "opsgenie_team" "sample_team" {
+  name        = "sample-team"
+  description = "sample team managed by terraform"
+
+  member {
+    id   = "${opsgenie_user.test_admin_user.id}"
+    role = "admin"
+  }
+
+  member {
+    id   = "${opsgenie_user.test_user.id}"
+    role = "user"
+  }
+}
+
+resource "opsgenie_team" "ui_only_team" {
+  name           = "UI Only Team"
+  description    = "Membership in this team is managed via OpsGenie web UI only"
+  ignore_members = true
+  delete_default_resources = true
+}
